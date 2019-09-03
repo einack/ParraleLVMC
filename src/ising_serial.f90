771b55f6389bd20ff04bac998254126562122136
Elliot Menkah <elliotsmenkah@gmailcom>
2019-09-03 22:59:27 +0200

    Added timings

--- src/ising_serial.f90
+++ src/ising_serial.f90
@@ -10,22 +10,22 @@ PROGRAM  isingmodel
     Implicit none
 
     integer :: ipar
-    real(8) ,dimension(3)    :: rn
     real(8)    :: rn1, rn2
     REAL(8) :: eecum, eecum2, ecum1, ebavg,eebavg,eebavg2 
     INTEGER :: ecount, ecount2, ibavg,iibavg,isgd
     real(8) :: sigma,learning_rate
     real(8) :: energy,energy_err
     real(8), dimension(3):: der, alpha
+    real(8) :: timeinit, timef, time1, time2, time3, time4 
+
+
+    call cpu_time(timeinit)
 
     OPEN(unit=8,File='energy.dat',Status='unknown')
     OPEN(unit=9,File='energy_opt.dat',Status='unknown')
     OPEN(unit=10,File='optimized.dat',Status='unknown')
     OPEN(unit=11,File='poptimized.dat',Status='unknown')
 
-    write(*,*) "here here 0"
-    write(*,*) "Debug, idum", a, idum
-    stop 0  ! Code stops here
 
     ! Initialize quantities 
     CALL initialize()
@@ -36,12 +36,13 @@ PROGRAM  isingmodel
     do ipar = 1, 3
         rn1 = ran2(idum)
         rn2 = ran2(idum)
-        rn(ipar) = SQRT(-2.d0*(sigma**2)*LOG(1.d0-rn1))*sin(2*pi*rn2)
+        alpha(ipar) = SQRT(-2.d0*(sigma**2)*LOG(1.d0-rn1))*sin(2*pi*rn2)
     end do
 
-    alpha(1) = dble(rn(1))
-    alpha(2) = dble(rn(2))
-    alpha(3) = dble(rn(3))
+
+    !alpha(1) = dble(rn(1))
+    !alpha(2) = dble(rn(2))
+    !alpha(3) = dble(rn(3))
 
     ibavg  = 0
     iibavg = 0
@@ -89,13 +90,23 @@ PROGRAM  isingmodel
 
     PRINT*, 'Begin optimization' 
     learning_rate = mu
- 
     ecount  = 1
     ecount2 = 0
     eecum   =  ecum1/(DBLE(Nspins*nwalk))
     eecum2  = (ecum1/(DBLE(Nspins*nwalk)))**2.d0
- 
+
+    call cpu_time(time1)
+
+    call cpu_time(time2)
+
     call vmc(alpha(1),alpha(2),alpha(3),energy,energy_err,der)
+
+    call cpu_time(time3)
+
+
+    !write(*,*) "here here 0"
+    !write(*,*) "Debug, alph", alpha
+    !stop 0  ! Code stops here
 
     do isgd = 1, nstep2
         call sgd(alpha(1),alpha(2),alpha(3),energy,energy_err,der,learning_rate)
@@ -109,14 +120,26 @@ PROGRAM  isingmodel
 
         end if
     end do
+
+    call cpu_time(time4)
+    
   
     print*, "End optimization"
 
     CLOSE(8)  
     CLOSE(10)
     CLOSE(11)
+    
+    call cpu_time(timef)
+    write(*,fmt=771)
 
+    write(*,fmt=777)'1', 'Before vmc', time1-timeinit 
+    write(*,fmt=777)'2', 'Duration of vmc', time3-time2 
+    write(*,fmt=777)'3', 'Duration of sgd:', time4-time3 
+    write(*,fmt=777)'4', 'Total Runtime: ', timef-timeinit 
 
+771 format('# No',1x,'Function',1x,'Duration')    
+777 format(1a,1x,a15,1x,f12.6, 'secs')
 END PROGRAM isingmodel
 
 ! ********************************************************************************************
